b18ff7a1c4e47ebbbbdda561f76168274aba0f8e
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-06-19 12:33:38 -0700

    Dev changes.

---
+++ CMD_production.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+ECHO ----------------
+ECHO 1 - [ production - npm run webpack ]
+ECHO 2 - [ production - npm run build ]
+ECHO ----------------
+
+SET /P input="ENTER: "
+
+IF %input% == 1 (
+    CALL npm run webpack
+)
+
+IF %input% == 2 (
+    CALL npm run build
+)
+
+ECHO ----------------
+
+ECHO FINISHED
+
+PAUSE
