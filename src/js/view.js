8d9459c35af3518c92a3c462857dbdd0b9af9f37
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-06-24 16:24:23 -0700

    Disallow selection of forecast summaries.

--- src/js/view.js
+++ src/js/view.js
@@ -2,7 +2,7 @@ import { getWeekday } from './dates';
 
 export const DayForecastSummary = function(day, i) {
     return `
-        <div class='weather-list-item d-inline-block text-bg-secondary border border-light' style='--bs-border-opacity: 0.1875;' data-index='${i}'>
+        <div class='weather-list-item d-inline-block user-select-none text-bg-secondary border border-light' style='--bs-border-opacity: 0.1875;' data-index='${i}'>
             <h2>${day.dt.getMonth()} / ${day.dt.getDate()}</h2>
             <h3>${getWeekday(day.dt)}</h3>
             <h3>${day.minTemp}&deg;F | ${day.maxTemp}&deg;F</h3>
