aebd5365e587a0ac382bf4fbd053d98359d3dce0
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-06-26 20:02:48 -0700

    Fix undefined error.

--- src/js/components/Component.js
+++ src/js/components/Component.js
@@ -2,7 +2,9 @@ export default class Component {
     getNodeChildrenEventHandler(target, elem, fn) {
         const children = [...elem.children];
         const container = children.filter(child => child.contains(target))[0];
-        const data = container.dataset;
+        const data = container?.dataset;
+
+        if (!data) return false;
 
         fn(data);
     }
