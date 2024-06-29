faa6922d66cc8c1d2d2db63e998a018faf623866
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-06-29 14:19:18 -0700

    Fix unused variable warnings.

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.scss';
 // import '../css/input.css';
 import Controller from './controller';
-// import JSX_Tests from './jsx_tests';
+import JSX_Tests from './jsx_tests';
 
 var controller;
-// var jsx_tests;
+var jsx_tests;
 
-window.onload = () => controller = new Controller();
-// window.Controller = controller;
+window.onload = () => new Controller();
+window.Controller = controller;
 
 // window.onload = () => jsx_tests = new JSX_Tests();
 // window.JSX_Tests = jsx_tests;
