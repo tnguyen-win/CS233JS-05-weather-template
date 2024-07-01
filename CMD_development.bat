bc16645cda261e0c7c45adb40ecdde2e433bc05a
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-07-01 15:46:12 -0700

    Initial publish of dev modules.

--- CMD_development.bat
+++ CMD_development.bat
@@ -4,30 +4,37 @@ ECHO ----------------
 ECHO 1 - [ development - npm run watch:webpack ]
 ECHO 2 - [ development - npm run lint ]
 ECHO 3 - [ development - npm run lint-fix ]
-ECHO 4 - [ development - npm run watch:tailwindcss ]
+ECHO 8 - [ development - publish to NPMJS - lib-component ]
+ECHO 9 - [ development - publish to NPMJS - lib-date2  ]
 ECHO 10 - [ development - npm run git-reset ]
 ECHO ----------------
 
 SET /P input="ENTER: "
 
 IF %input% == 1 (
-	CALL npm run watch:webpack
+    CALL npm run watch:webpack
 )
 
 IF %input% == 2 (
-	CALL npm run lint
+    CALL npm run lint
 )
 
 IF %input% == 3 (
-	CALL npm run lint-fix
+    CALL npm run lint-fix
 )
 
-IF %input% == 4 (
-	CALL npm run watch:tailwindcss
+IF %input% == 8 (
+    CALL CD "%CD%\dev_modules\@ocdla\component"
+    CALL npm publish --access=public
+)
+
+IF %input% == 9 (
+    CALL CD "%CD%\dev_modules\@ocdla\date2"
+    CALL npm publish --access=public
 )
 
 IF %input% == 10 (
-	CALL npm run git-reset
+    CALL npm run git-reset
 )
 
 ECHO ----------------
