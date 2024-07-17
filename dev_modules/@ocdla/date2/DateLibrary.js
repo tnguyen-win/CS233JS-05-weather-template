fbd090dff09413f2dfb0a2c2cd762de6345defcf
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-07-17 11:29:28 -0700

    Removed no longer needed code.

--- dev_modules/@ocdla/date2/DateLibrary.js
+++ dev_modules/@ocdla/date2/DateLibrary.js
@@ -28,31 +28,6 @@ function zeroPad(num) {
     return sign === -1 ? '-' + str : str;
 }
 
-export function toString(unixTimestamp, timezoneOffsetInSeconds, locale) {
-    let date = new Date(unixTimestamp * 1000);
-    let options = {};
-    let hoursOffset = toHours(timezoneOffsetInSeconds);
-    let utcHours = zeroPad(hoursOffset);
-
-    options.timeZone = utcHours;
-
-    return date.toLocaleString(locale, options);
-
-    // const hours = date.getHours();
-    // const minutes = date.getMinutes();
-    // const seconds = date.getSeconds();
-    // const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-    // const ampm = hours >= 12 ? 'PM' : 'AM';
-    // const timeString =
-    //     formattedHours + ':' + minutes + ':' + seconds + ' ' + ampm;
-
-    // return timeString;
-}
-
-// function parse(str) {
-//     return str.split();
-// }
-
 function getOptions(timezoneOffsetInSeconds) {
     let options = {};
     let hoursOffset = toHours(timezoneOffsetInSeconds);
