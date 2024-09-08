e00412ede368f1e411627b59e22df3749f2dc437
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-09-08 13:28:33 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -87,8 +87,8 @@ module.exports = {
                 //     from: path.resolve(__dirname, 'src/images'),
                 //     to: path.resolve(__dirname, 'dist/images')
                 // }
-                'src/.nojekyll',
-                { from: './src/js/sw.js', to: './sw.js' }
+                'src/.nojekyll'
+                // { from: './src/js/sw.js', to: './sw.js' }
             ]
         })
     ]
