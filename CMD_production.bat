b8c59574087950a22c8d9c2ff4eb5b5c9d08f4cb
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-09-08 12:04:06 -0700

    More production fixes.

--- CMD_production.bat
+++ CMD_production.bat
@@ -2,12 +2,17 @@
 
 ECHO ----------------
 ECHO 1 - [ production - npm run build ]
+ECHO 2 - [ production - npm run serve ]
 ECHO ----------------
 
 SET /P input="ENTER: "
 
 IF %input% == 1 (
     CALL npm run build
+)
+
+IF %input% == 2 (
+    CALL npm run serve
 )
 
 ECHO ----------------
